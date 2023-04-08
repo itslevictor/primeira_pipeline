1da2081256cdde3c4b950fb99734d89e381d9b91
Victor <adm.victorfernando@gmail.com>
2023-04-07 23:33:12 -0300

    Update pipeline-react-app.sh

--- pipeline-react-app.sh
+++ pipeline-react-app.sh
@@ -25,10 +25,12 @@ else
 echo
 mkdir /app
 # SE NAO SATISFAZ ENTRA AQUI
-curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
-sudo yum install -y nodejs
-sudo npm install -g n
-sudo n latest
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
+
+export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+yum install -y nodejs
+
 fi
 
 if [ -d repository/ ];
