71be109076749925b1887d2bf74b62a7c0cdb556
Victor <adm.victorfernando@gmail.com>
2023-04-09 19:03:31 -0300

    Update pipeline-react-app.sh

--- pipeline-react-app.sh
+++ pipeline-react-app.sh
@@ -4,13 +4,13 @@ echo
 #checkout
 if [ -e  /usr/lib/systemd/system/react-app.service ];
 then
-# SE SATISFAZ ENTRA AQUI
+echo
+# SE SATISFAZ ENTRA AQUI - remove e move limpando o git
 rm -rf /usr/lib/systemd/system/react-app.service
 mv /primeira_pipeline/react-app.service /usr/lib/systemd/system
 rm -rf primeira_pipeline
 else
 echo
-mkdir /app
 # SE NAO SATISFAZ ENTRA AQUI
 mv /primeira_pipeline/react-app.service /usr/lib/systemd/system
 rm -rf primeira_pipeline
@@ -19,59 +19,60 @@ fi
 
 if [ -d /usr/local/lib/node_modules ];
 then
-# SE SATISFAZ ENTRA AQUI
 echo
+# SE SATISFAZ ENTRA AQUI
 else
 echo
-mkdir /app
 # SE NAO SATISFAZ ENTRA AQUI
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.2/install.sh | bash
-
-export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-nvm install node
-nvm install 8.0.0
-sudo yum update
+curl -sL https://rpm.nodesource.com/setup_16.x | sudo bash -
+yum install -y nodejs
 fi
 
 if [ -d repository/ ];
 then
+echo
 # SE SATISFAZ ENTRA AQUI
 echo entrando no repositorio e removendo a pasta a força
+rm -rf repository
+mkdir repository
 echo
-cd /repository
-rm -rf react-app
 else
 echo criada a pasta repository
 echo
-mkdir /repository
 # SE NAO SATISFAZ ENTRA AQUI
+mkdir /repository
 fi
 
 if [ -d app/ ];
 then
-echo
 # SE SATISFAZ ENTRA AQUI
+rm -rf app
+mkdir app
+echo Criada a pasta app
+echo
 else
+# SE NAO SATISFAZ ENTRA AQUI
 echo criada a pasta app
 echo
-mkdir /app
-# SE NAO SATISFAZ ENTRA AQUI
+mkdir app
 fi
-
-echo iniciando o conteudo do sh
+echo iniciando o conteudo do .sh
 echo
 
 echo baixando e instalando pasta
 echo
+cd repository
 git clone https://github.com/kelvin-alef/react-app.git
+cd react-app
+npm install
+sudo apt install xsel 
 
 echo compilando
 echo
 
 npm run build
 	
-#deplyando
+#deployando
 echo iniciando deploy
 echo
 
@@ -83,4 +84,3 @@ rm -rf /app/react-app
 mv /repository/react-app/build /app/react-app
 
 #encerra aqui o shell
-
